c56a3019cfe2b975e55f123676def63a2d3f6046
Fernão Lopes <fernao@riseup.net>
2016-11-21 18:52:22 -0200

    link para ponto funcionando - sem estilo

--- wp-autenticacao-ad.php
+++ wp-autenticacao-ad.php
@@ -375,14 +375,15 @@ class Wp_Autenticacao_Ad_Widget extends WP_Widget {
         
 		// outputs the content of the widget
         if (wp_autenticacao_ad_is_logado()) {
-   $usuario = wp_autenticacao_ad_get_usuario();
+            $usuario = wp_autenticacao_ad_get_usuario();
 ?>
 	<div class="caixa_bem_vindo">	
           <div>
 	    <span><?php echo get_option('texto_logon'); ?>, <?php echo $usuario->getApelido() ?></span>
           </div>
-	  <br clear="all" />
-	  
+          <br clear="all" />
+          <a href="http://intra/srh/ponto/ctrRegistraFrequencia/ctrRegistraFrequencia.php?sLogon=<?php echo $usuario->getLogin() ?>&sIp=<?php echo wp_autenticacao_ad_get_ip(); ?>">ponto</a>
+          <br clear="all" /><br clear="all" />
           <form action="<?php echo wp_autenticacao_ad_get_url() ?>" method="post" class="caixa_bem_vindo">
 	    <input type="hidden" name="acao" value="logout"/>
             <input type="submit" value="Sair" class="txtIndent" />
